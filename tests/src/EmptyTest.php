0a70f4b5c70e1900deac65b15bbb01077f5fffe6
Giorgio Sironi <g.sironi@elifesciences.org>
2017-10-10 15:53:02 +0000

    Attempting to add a test

--- tests/src/EmptyTest.php
+++ tests/src/EmptyTest.php
@@ -1,9 +1,27 @@
 <?php
 
+use Silex\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+
 class EmptyTest extends \PHPUnit_Framework_TestCase
 {
-    public function testThisIsJustAPlaceholderForRealTestsToCheckTheWholeBuildIsRunningCorrectly()
+    /**
+     * @before
+     */
+    public function setUpApp()
     {
-        $this->assertTrue(true);
+        // bootstrap should return a Kernel object
+        // to be used by tests or web/app_*.php files
+        $this->app = require __DIR__.'/../../web/bootstrap.php';
+    }
+
+    public function testPing()
+    {
+        $response = $this->app->handle(Request::create('/ping'));
+        $this->assertSame(
+            200, 
+            $response->getStatusCode(),
+            var_export(json_decode($response->getContent(), true), true)
+        );
     }
 }
